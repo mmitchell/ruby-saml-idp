7027f78c71c52b9f73f16d183403dfb2e99a21bd
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2015-06-29 03:35:15 -0700

    Updated for testing project.

--- app/controllers/saml_idp/idp_controller.rb
+++ app/controllers/saml_idp/idp_controller.rb
@@ -14,8 +14,8 @@ module SamlIdp
     end
 
     def create
-      unless params[:email].blank? && params[:password].blank?
-        person = idp_authenticate(params[:email], params[:password])
+      if params[:enterprise_id].present? && params[:email].present? && params[:first_name].present? && params[:last_name].present?
+        person = idp_authenticate(params[:enterprise_id], params[:email], params[:first_name], params[:last_name], params[:is_employee?])
         if person.nil?
           @saml_idp_fail_msg = "Incorrect email or password."
         else
